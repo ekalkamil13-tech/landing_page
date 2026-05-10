31aa774e11b5845f184b2314efa9b92ee27a24a7
ekalkamil13-tech <ekalkamil.13@gmail.com>
2026-05-10 20:29:20 +0700

    Update HomeController.php

--- Controller/HomeController.php
+++ Controller/HomeController.php
@@ -1,1 +1,13 @@
 
+<?php
+require_once 'Model/RentalModel.php';
+
+class HomeController {
+    public function index() {
+        $model = new RentalModel();
+        $prices = $model->getPrices();
+        
+        require_once 'View/home.php';
+    }
+}
+?>
